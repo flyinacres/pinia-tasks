a20ecbf14e133d120923e9f8b3b47284a97c0cb8
flyinacres <ron-github@rfgf.com>
2023-02-06 09:30:38 -0800

    Added actions

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -3,14 +3,29 @@
         <h3>{{ task.title }}</h3>
         <div class="icons">
             <!-- Selecting this class type relates the string name to an icon -->
-            <i class="material-icons">delete</i>
-            <i class="material-icons">favorites</i>
+            <i 
+                class="material-icons"
+                @click="taskStore.deleteTask(task.id)">
+                delete
+            </i>
+            <i 
+                class="material-icons"
+                @click="taskStore.toggleFav(task.id)">
+                favorites
+            </i>
         </div>
     </div>
 </template>
 
 <script>
+    import { useTaskStore } from '@/stores/TaskStore'
+
     export default {
-        props: ['task']
+        props: ['task'],
+        setup() {
+            const taskStore = useTaskStore()
+
+            return { taskStore }
+        }
     }
 </script>
