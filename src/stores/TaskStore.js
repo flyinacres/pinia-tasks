6265082d26d2a59f508b5549bca5675613923ff9
flyinacres <ron-github@rfgf.com>
2023-02-06 09:19:01 -0800

    First action

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -20,5 +20,10 @@ export const useTaskStore = defineStore('taskStore', {
         totalCount: (state) => {
             return state.tasks.length
         }
+    },
+    actions: {
+        addTask(task) {
+            this.tasks.push(task)
+        }
     }
 })
